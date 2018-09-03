09943e3307c76ab3c4a8531f0364d8d81677eb88
HuwGrenfell <huw.grenfell@gmail.com>
2018-09-03 19:08:25 +1000

    Initial commit

---
+++ resources/views/vendor/adminlte/home.blade.php
@@ -0,0 +1,243 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	{{ trans('adminlte_lang::message.home') }}
+@endsection
+
+
+@section('main-content')
+
+    <div class="container-fluid spark-screen">
+
+        <h1 class="page-header">Topics</h1>
+
+        <!-- START TOPIC ROW -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-maroon">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-aqua">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-green">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-purple">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+        <!-- START TOPIC ROW -->
+        <div class="row">
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-light-blue">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-teal">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-navy">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3">
+                <div class="box box-widget widget-user">
+                    <div class="widget-user-header bg-orange">
+                        <h3 class="widget-user-username">Tax</h3>
+                        <br>
+                        <br>
+                        <h5 class="widget-user-desc">Learn the basics about how tax works</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+
+
+        <!-- START TIMELINE -->
+        <div class="row">
+            <section class="content-header">
+                <h1>
+                    Widgets
+                    <small>Preview page</small>
+                </h1>
+            </section>
+        </div>
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12 col-md-offset-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text"><strong>Application</strong></span>
+                            {{--<span class="info-box-number">410</span>--}}
+                            <br>
+                            Application successfully recieved
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div>
+        <!-- /.row -->
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12 col-md-offset-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text"><strong>Application</strong></span>
+                            {{--<span class="info-box-number">410</span>--}}
+                            <br>
+                            Application successfully recieved
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12 col-md-offset-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text"><strong>Application</strong></span>
+                            {{--<span class="info-box-number">410</span>--}}
+                            <br>
+                            Application successfully recieved
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </section>
+
+        {{--<!-- START CAROUSEL-->--}}
+        {{--<div class="row">--}}
+            {{--<div class="row">--}}
+                {{--<!-- /.col -->--}}
+                {{--<div class="col-md-6 col-md-offset-6">--}}
+                    {{--<div class="box box-solid">--}}
+                    {{--<div class="box-header with-border">--}}
+                    {{--<h3 class="box-title">Carousel</h3>--}}
+                    {{--</div>--}}
+                    {{--<!-- /.box-header -->--}}
+                        {{--<div class="box-body">--}}
+                            {{--<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">--}}
+                                {{--<ol class="carousel-indicators">--}}
+                                    {{--<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>--}}
+                                    {{--<li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>--}}
+                                    {{--<li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>--}}
+                                {{--</ol>--}}
+                                {{--<div class="carousel-inner">--}}
+                                    {{--<div class="item active">--}}
+                                        {{--<img src="http://placehold.it/1200x500/39CCCC/ffffff&text=Article+1" alt="First slide">--}}
+
+                                        {{--<div class="carousel-caption">--}}
+                                            {{--First Slide--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                    {{--<div class="item">--}}
+                                        {{--<img src="http://placehold.it/1200x500/3c8dbc/ffffff&text=Article+2" alt="Second slide">--}}
+
+                                        {{--<div class="carousel-caption">--}}
+                                            {{--Second Slide--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                    {{--<div class="item">--}}
+                                        {{--<img src="http://placehold.it/1200x500/f39c12/ffffff&text=Article+3" alt="Third slide">--}}
+
+                                        {{--<div class="carousel-caption">--}}
+                                            {{--Third Slide--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">--}}
+                                    {{--<span class="fa fa-angle-left"></span>--}}
+                                {{--</a>--}}
+                                {{--<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">--}}
+                                    {{--<span class="fa fa-angle-right"></span>--}}
+                                {{--</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<!-- /.box-body -->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.box -->--}}
+                {{--</div>--}}
+                {{--<!-- /.col -->--}}
+            {{--</div>--}}
+            {{--<!-- /.row -->--}}
+            {{--<!-- END ACCORDION & CAROUSEL-->--}}
+        {{--</div>--}}
+        {{--<!-- /.row -->--}}
+
+
+    </div>
+
+@endsection
